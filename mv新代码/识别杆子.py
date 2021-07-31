3fb7fef9402880e709c06f68f314a38a563f085f
aface0427 <aface0427@163.com>
2021-07-31 15:25:06 +0800

    Create 识别杆子.py

---
+++ mv新代码/识别杆子.py
@@ -0,0 +1,73 @@
+# 线段检测例程
+#
+# 这个例子展示了如何在图像中查找线段。对于在图像中找到的每个线对象，
+# 都会返回一个包含线条旋转的线对象。
+
+# find_line_segments()找到有限长度的线（但是很慢）。
+# Use find_line_segments()找到非无限的线（而且速度很快）。
+
+enable_lens_corr = 1 # turn on for straighter lines...打开以获得更直的线条…
+
+import sensor, image, time
+
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE) # 灰度更快
+sensor.set_framesize(sensor.QQVGA)
+sensor.skip_frames(time = 2000)
+clock = time.clock()
+min_degree=-5
+max_degree=5
+# 所有线段都有 `x1()`, `y1()`, `x2()`, and `y2()` 方法来获得他们的终点
+# 一个 `line()` 方法来获得所有上述的四个元组值，可用于 `draw_line()`.
+T=0
+mx=0
+mn=9999
+lmx=70
+lmn=90
+def maxx(a,b):
+    if(a>b):
+        return a
+    else:
+        return b
+def minn(a,b):
+    if(a<b):
+        return a
+    else:
+        return b
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    img.laplacian(1,sharpen=True)
+    if enable_lens_corr: img.lens_corr(1.8) # for 2.8mm lens...
+
+    # `merge_distance`控制附近行的合并。 在0（默认），没有合并。
+    # 在1处，任何距离另一条线一个像素点的线都被合并...等等，
+    # 因为你增加了这个值。 您可能希望合并线段，因为线段检测会产生大量
+    # 的线段结果。
+
+    # `max_theta_diff` 控制要合并的任何两线段之间的最大旋转差异量。
+    # 默认设置允许15度。
+    p=0
+    a=[0.0 for i in range(1,100)]
+    for l in img.find_line_segments(roi=(70,0,20,120),merge_distance = 0, max_theta_diff = 5):
+        if (min_degree <= l.theta()) and (l.theta() <= max_degree):
+            p=p+1
+            a[p]=l.x1()
+            mx=maxx(mx,a[p])
+            mn=minn(mn,a[p])
+            img.draw_line(l.line(), color = (255, 0, 0))
+        # print(l)
+    if(p==0 or p<8):
+        continue
+    ave=0
+    for i in range(1,p):
+        ave=ave+a[i]
+    ave=ave/p
+    #ave应在80处，lmx-lmn应等于15左右
+    print(ave,lmn,lmx)
+    if(T%3==0):
+        lmx=mx
+        lmn=mn
+        mx=0
+        mn=9999
+    print("FPS %f" % clock.fps())
